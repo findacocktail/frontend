905e4928d67ab3ca8b53fd8cdc7e114cc8a402ff
Ramon Medeiros <ramon@juni.co>
2024-12-23 19:18:04 +0300

    set https mandatory

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://findacocktail.com"
+  base:"https://findacocktail.com",
+  server:{
+    https: true
+  }
 })
