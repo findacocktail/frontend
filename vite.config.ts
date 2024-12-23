d294d962e3de57fb45aa94ad7aada705358f4fa1
Ramon Medeiros <ramon@juni.co>
2024-12-23 19:26:12 +0300

    Use github domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://findacocktail.com",
-  server:{
+  base: "https://findacocktail.github.io/frontend",
+  server: {
     https: true
   }
 })
