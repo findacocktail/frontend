9ee7b5232659fd89584379b6251f0adda91bc11e
Ramon Medeiros <ramon@juni.co>
2025-01-09 12:06:23 +0100

    general changes
    - change on css
    
    - add lint to code
    
    - add google tag manager

--- src/data/cocktails.tsx
+++ src/data/cocktails.tsx
@@ -1,72 +1,63 @@
 import { useEffect, useState } from "react";
 
-async function getCocktails(terms: string[]) : Promise<Cocktail[]>{
-    let requestURL = new URL("https://api.findacocktail.com/cocktails")
+async function getCocktails(terms: string[]): Promise<Cocktail[]> {
+  let requestURL = new URL("https://api.findacocktail.com/cocktails");
 
-    for (let i = 0; i < terms.length; i++) {
-        requestURL.searchParams.append("term", terms[i])
-    }
+  for (let i = 0; i < terms.length; i++) {
+    requestURL.searchParams.append("term", terms[i]);
+  }
 
-    /*
-    for (let term in notIncluded) {
-        requestURL.searchParams.append("notIncluded", term)
-    }
-    */
+  const response = await fetch(requestURL, { method: "GET" });
 
-    const response = await fetch(requestURL, {
-        method: "GET"
-    })
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
 
-    if (!response.ok) {
-        throw new Error("Network response was not ok")
-    }
-
-    return await response.json() as Promise<Cocktail[]>
+  return (await response.json()) as Promise<Cocktail[]>;
 }
 
 export const useCocktails = (terms: string[]) => {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<Error | undefined>();
-    const [cocktails, setCocktails] = useState<Cocktail[] | undefined>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | undefined>();
+  const [cocktails, setCocktails] = useState<Cocktail[] | undefined>();
 
-    useEffect(() => {
-        const fetchCocktails = async () => {
-            try {
-                if (terms.length == 0){
-                    return
-                }
-                setLoading(true);
-                const data = await getCocktails(terms)
-                setCocktails(data)
-            } catch (e) {
-                setError(e as Error)
-            } finally {
-                setLoading(false)
-            }
+  useEffect(() => {
+    const fetchCocktails = async () => {
+      try {
+        if (terms.length == 0) {
+          return;
         }
+        setLoading(true);
+        const data = await getCocktails(terms);
+        setCocktails(data);
+      } catch (e) {
+        setError(e as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchCocktails()
-    }, [terms])
+    fetchCocktails();
+  }, [terms]);
 
-    return {
-        loading,
-        error,
-        cocktails,
-    }
-}
+  return {
+    loading,
+    error,
+    cocktails,
+  };
+};
 
 export interface Cocktail {
-    name: string
-    youtube_link: string
-    ingredients: Ingredient[]
-    method: string
-    garnish: string
-    image_url: string
+  name: string;
+  youtube_link: string;
+  ingredients: Ingredient[];
+  method: string;
+  garnish: string;
+  image_url: string;
 }
 
 export interface Ingredient {
-    amount: number
-    scale: string
-    description: string
+  amount: number;
+  scale: string;
+  description: string;
 }
-
