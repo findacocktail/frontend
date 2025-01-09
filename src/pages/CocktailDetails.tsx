e71b297ce623a3bbcba5544598f2fb516960d581
Ramon Medeiros <ramon@juni.co>
2025-01-09 15:48:02 +0100

    feat: add base page for cocktail details

--- src/pages/CocktailDetails.tsx
+++ src/pages/CocktailDetails.tsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router";
 import { useCocktail } from "../data/cocktail.ts";
-import { LinearProgress } from "@mui/joy";
+import { Card } from "@mui/joy";
+import { CardMedia, Typography } from "@mui/material";
+import Divider from "@mui/material/Divider";
 
 export default function CocktailDetails() {
   let { name } = useParams();
@@ -10,10 +12,49 @@ export default function CocktailDetails() {
     console.log(error);
   }
 
+  if (loading) {
+    console.log("loading");
+  }
+
+  if (!cocktail) {
+    return;
+  }
+
   return (
-    <div>
-      {loading && <LinearProgress thickness={1} />}
-      <h1>{cocktail?.youtube_link}</h1>
-    </div>
+    <Card sx={{paddingLeft:"20%", paddingRight:"20%"}}>
+      <Typography variant="h3">{cocktail.name}</Typography>
+
+      <CardMedia
+        component="img"
+        sx={{ height: 250, width: 150, display: "inline" }}
+        image={cocktail.image_url}
+        alt={cocktail.name}
+      />
+
+      <Divider />
+
+      {/* Ingredients Section */}
+      <div>
+        <Typography variant="h4">Ingredients</Typography>
+        <h2 className="text-xl font-semibold mb-3"></h2>
+        <ul className="space-y-2">
+          {cocktail.ingredients.map((ingredient, index) => (
+            <li key={index} className="text-gray-700">
+              {`${ingredient.amount} ${ingredient.scale} ${ingredient.description}`}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <Divider />
+
+      {/* Steps Section */}
+      <div>
+        <Typography variant="h4">Instructions</Typography>
+        <ol className="space-y-2">
+          <li className="text-gray-700">{cocktail.method}</li>
+        </ol>
+      </div>
+    </Card>
   );
 }
