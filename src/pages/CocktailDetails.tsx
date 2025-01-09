2dff63d20fa498d4274a3c4e4edbe5814ff691a0
Ramon Medeiros <ramon@juni.co>
2025-01-09 17:50:52 +0100

    fix: improve overall design on cocktail details

--- src/pages/CocktailDetails.tsx
+++ src/pages/CocktailDetails.tsx
@@ -1,7 +1,6 @@
 import { useParams } from "react-router";
 import { useCocktail } from "../data/cocktail.ts";
-import { Card } from "@mui/joy";
-import { CardMedia, Typography } from "@mui/material";
+import { CardMedia, Paper } from "@mui/material";
 import Divider from "@mui/material/Divider";
 
 export default function CocktailDetails() {
@@ -9,52 +8,54 @@ export default function CocktailDetails() {
   const { loading, error, cocktail } = useCocktail(name as string);
 
   if (error) {
-    console.log(error);
+    console.error(error);
   }
 
   if (loading) {
     console.log("loading");
   }
 
-  if (!cocktail) {
-    return;
-  }
-
   return (
-    <Card sx={{paddingLeft:"20%", paddingRight:"20%", position:"relative"}}>
-      <Typography variant="h3">{cocktail.name}</Typography>
+    <Paper
+      elevation={0}
+      sx={{ paddingLeft: "20%", paddingRight: "20%", position: "relative" }}
+    >
+      <h1>{cocktail?.name}</h1>
 
       <CardMedia
         component="img"
-        sx={{ height: 250, width: 150, display: "inline" }}
-        image={cocktail.image_url}
-        alt={cocktail.name}
+        sx={{ height: 250, width: 150, display: "inline", marginLeft:"4%",marginTop:"4%" }}
+        image={cocktail?.image_url}
+        alt={cocktail?.name}
       />
 
       <Divider />
 
       {/* Ingredients Section */}
-      <div>
-        <Typography variant="h4">Ingredients</Typography>
-        <h2 className="text-xl font-semibold mb-3"></h2>
-        <ul className="space-y-2">
-          {cocktail.ingredients.map((ingredient, index) => (
-            <li key={index} className="text-gray-700">
-              {`${ingredient.amount} ${ingredient.scale} ${ingredient.description}`}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Paper elevation={0} sx={{ padding: "4% 4% 0 0 " }}>
+        <h1>Ingredients</h1>
+        <Paper elevation={0} sx={{ padding: "4% 4%" }}>
+          <ul>
+            {cocktail?.ingredients.map((ingredient, index) => (
+              <li key={index}>
+                {`${ingredient.amount} ${ingredient.scale} ${ingredient.description}`}
+              </li>
+            ))}
+          </ul>
+        </Paper>
+      </Paper>
 
       <Divider />
 
       {/* Steps Section */}
-      <div>
-        <Typography variant="h4">Instructions</Typography>
-        <ol className="space-y-2">
-          <li className="text-gray-700">{cocktail.method}</li>
-        </ol>
-      </div>
-    </Card>
+      <Paper elevation={0} sx={{ padding: "4% 4% 0 0" }}>
+        <h1>Instructions</h1>
+        <Paper elevation={0} sx={{ padding: "4% 4%" }}>
+          <ol>
+            <li>{cocktail?.method}</li>
+          </ol>
+        </Paper>
+      </Paper>
+    </Paper>
   );
 }
